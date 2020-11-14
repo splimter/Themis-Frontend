2b30ca442ee823b1f4a2d67d4b73c509bc501005
Merah Soheyb <soheybemir@hotmail.fr>
2020-11-14 10:29:19 +0000

    goffing around

--- src/services/base.js
+++ src/services/base.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import dotenv from 'dotenv'
 dotenv.config()
-console.log(process.env)
+
 export const http = axios.create({
     baseURL: process.env.REACT_APP_SERVER_URL
 });
